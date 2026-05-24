bbffef505e12caf47c81c21777f526ad31da9474
Zenithsui7 <shreeramorange444@gmail.com>
2026-05-24 21:49:00 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey:            "YOUR_API_KEY",
-  authDomain:        "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId:         "YOUR_PROJECT_ID",
-  storageBucket:     "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId:             "YOUR_APP_ID",
+  apiKey:            "AIzaSyBCpw83tcyGHNXJmiDKiaYQkKXKoUsL-H4",
+  authDomain:        "label-sanj.firebaseapp.com",
+  projectId:         "label-sanj",
+  storageBucket:     "label-sanj.firebasestorage.app",
+  messagingSenderId: "82906631288",
+  appId:             "1:82906631288:web:cb96efb7e65b492f65318d",
 };
 
 // Detect placeholder / unconfigured values
